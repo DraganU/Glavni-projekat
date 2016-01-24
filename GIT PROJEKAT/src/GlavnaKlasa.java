c4c5f1032353cc49b76b3bf291bf18da033b25e4
Dragan <dragan.ujfalusi@gmail.com>
2016-01-24 14:01:30 +0100

    druga

--- GIT PROJEKAT/src/GlavnaKlasa.java
+++ GIT PROJEKAT/src/GlavnaKlasa.java
@@ -3,7 +3,12 @@ public class GlavnaKlasa {
 
 	public static void main(String[] args) {
 		System.out.println("Ovo je glavna klasa!");
-
+		
+		System.out.println("dddddddd");
+		
+		
+		
+		
 	}
 
 }
